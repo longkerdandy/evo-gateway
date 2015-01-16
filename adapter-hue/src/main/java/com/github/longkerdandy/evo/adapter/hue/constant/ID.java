e6f6003f6a839c196d530b97a8e312d645d8aec5
LongkerDandy <longkerdandy@gmail.com>
2015-01-16 17:27:18 +0800

    Change hue light's device id get method

--- adapter-hue/src/main/java/com/github/longkerdandy/evo/adapter/hue/constant/ID.java
+++ adapter-hue/src/main/java/com/github/longkerdandy/evo/adapter/hue/constant/ID.java
@@ -3,29 +3,35 @@ package com.github.longkerdandy.evo.adapter.hue.constant;
 import com.fasterxml.uuid.EthernetAddress;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.NameBasedGenerator;
+import com.github.longkerdandy.evo.api.util.UuidUtils;
 import org.apache.commons.lang3.StringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * IDs
  */
 public class ID {
 
-    private static String gatewayId;
+    private static String gatewayId;    // Evolution Gateway's Device Id
+    private static String bridgeMac;    // Hue Bridge's Mac Address
+    private static String bridgeId;     // Hue Bridge's Device Id
 
     /**
      * Get Gateway's Device Id
      *
      * @return Gateway's Device Id
-     * @throws Exception If can't determine ethernet address
+     * @throws IllegalStateException If can't determine ethernet address
      */
-    public static String getGatewayId() throws Exception {
+    public static String getGatewayId() {
         if (StringUtils.isNotEmpty(gatewayId)) {
             return gatewayId;
         }
 
         EthernetAddress ea = EthernetAddress.fromInterface();
         if (ea == null) {
-            throw new Exception("Can't determine ethernet address");
+            throw new IllegalStateException("Can't determine ethernet address");
         }
         NameBasedGenerator generator = Generators.nameBasedGenerator(NameBasedGenerator.NAMESPACE_URL);
         gatewayId = generator.generate("https://github.com/longkerdandy/evo-gateway?mac=" + ea.toString()).toString();
@@ -38,7 +44,29 @@ public class ID {
      * @return Hue Adapter's Device Id
      */
     public static String getAdapterId() {
-        return gatewayId + "-hue";
+        return getGatewayId() + "-hue";
+    }
+
+    /**
+     * Get Hue Bridge's Device Id
+     *
+     * @param mac Bride's Mac Address
+     * @return Hue Bridge's Device Id
+     */
+    public static String getBridgeId(String mac) {
+        if (bridgeMac != null && bridgeMac.equals(mac)) {
+            return bridgeId;
+        }
+
+        try {
+            URI uri = new URI("http://www.meethue.com/bridge?mac=" + mac);
+            bridgeMac = mac;
+            bridgeId = UuidUtils.shortUuid(uri);
+        } catch (URISyntaxException ignore) {
+            // never happens
+        }
+
+        return bridgeId;
     }
 
     /**
@@ -47,22 +75,7 @@ public class ID {
      * @param lightId Light Id from HueSDK
      * @return Device Id used in Evolution protocol
      */
-    public static String fromLightId(String lightId) {
-        return gatewayId + "-hue-" + lightId;
-    }
-
-    /**
-     * Device Id -> Light Id
-     *
-     * @param deviceId Light Id from HueSDK
-     * @return Light Id from HueSDK
-     */
-    public static String toLightId(String deviceId) {
-        int index = deviceId.lastIndexOf("-hue-");
-        if (index >= 0 && deviceId.length() > index + 5) {
-            return deviceId.substring(index + 5);
-        } else {
-            return null;
-        }
+    public static String fromLightId(String mac, String lightId) {
+        return getBridgeId(mac) + "-" + lightId;
     }
 }
