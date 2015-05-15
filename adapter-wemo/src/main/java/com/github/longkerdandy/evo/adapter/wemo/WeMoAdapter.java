b933beaa03e97bc02b3e113b03850ae17837fed2
LongkerDandy <longkerdandy@gmail.com>
2015-05-15 17:39:30 +0800

    basic wemo fix with cling

--- adapter-wemo/src/main/java/com/github/longkerdandy/evo/adapter/wemo/WeMoAdapter.java
+++ adapter-wemo/src/main/java/com/github/longkerdandy/evo/adapter/wemo/WeMoAdapter.java
@@ -1,7 +1,96 @@
 package com.github.longkerdandy.evo.adapter.wemo;
 
+import com.github.longkerdandy.evo.adapter.wemo.upnp.WeMoUpnpServiceConfiguration;
+import org.fourthline.cling.UpnpService;
+import org.fourthline.cling.UpnpServiceImpl;
+import org.fourthline.cling.model.message.header.DeviceTypeHeader;
+import org.fourthline.cling.model.meta.LocalDevice;
+import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.model.types.DeviceType;
+import org.fourthline.cling.registry.Registry;
+import org.fourthline.cling.registry.RegistryListener;
+
 /**
- * Created by Dandy on 2015/5/13.
+ * Belkin WeMo adapter
  */
 public class WeMoAdapter {
+
+    public static void main(String[] args) throws Exception {
+
+        // UPnP discovery is asynchronous, we need a callback
+        RegistryListener listener = new RegistryListener() {
+
+            public void remoteDeviceDiscoveryStarted(Registry registry,
+                                                     RemoteDevice device) {
+                System.out.println(
+                        "Discovery started: " + device.getDisplayString()
+                );
+            }
+
+            public void remoteDeviceDiscoveryFailed(Registry registry,
+                                                    RemoteDevice device,
+                                                    Exception ex) {
+                System.out.println(
+                        "Discovery failed: " + device.getDisplayString() + " => " + ex
+                );
+            }
+
+            public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
+                System.out.println(
+                        "Remote device available: " + device.getDisplayString()
+                );
+            }
+
+            public void remoteDeviceUpdated(Registry registry, RemoteDevice device) {
+                System.out.println(
+                        "Remote device updated: " + device.getDisplayString()
+                );
+            }
+
+            public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
+                System.out.println(
+                        "Remote device removed: " + device.getDisplayString()
+                );
+            }
+
+            public void localDeviceAdded(Registry registry, LocalDevice device) {
+                System.out.println(
+                        "Local device added: " + device.getDisplayString()
+                );
+            }
+
+            public void localDeviceRemoved(Registry registry, LocalDevice device) {
+                System.out.println(
+                        "Local device removed: " + device.getDisplayString()
+                );
+            }
+
+            public void beforeShutdown(Registry registry) {
+                System.out.println(
+                        "Before shutdown, the registry has devices: "
+                                + registry.getDevices().size()
+                );
+            }
+
+            public void afterShutdown() {
+                System.out.println("Shutdown of registry complete!");
+
+            }
+        };
+
+        // This will create necessary network resources for UPnP right away
+        System.out.println("Starting Cling...");
+        UpnpService upnpService = new UpnpServiceImpl(new WeMoUpnpServiceConfiguration(), listener);
+
+        // Send a search message to all belkin wemo switch devices, they should respond soon
+        upnpService.getControlPoint().search(new DeviceTypeHeader(new DeviceType("Belkin", "controllee", 1)));
+
+        // Let's wait 10 seconds for them to respond
+        System.out.println("Waiting 10 seconds before shutting down...");
+        Thread.sleep(10000);
+
+        // Release all resources and advertise BYEBYE to other UPnP devices
+        System.out.println("Stopping Cling...");
+        upnpService.shutdown();
+    }
 }
